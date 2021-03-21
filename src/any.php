17d67f528e04cbfca6a257cd568c29a594a9d757
masterfermin02 <fperdomo@intellisys.com.do>
2021-03-21 01:00:08 -0400

    Fixed small issue

--- src/any.php
+++ src/any.php
@@ -10,15 +10,15 @@ namespace Slash;
  * @return bool
  *
  * @example
-	Slash\any([1, 3, 5], 'Slash\isOdd'); // === true
-
-	Slash\any([1, 3, 5], function ($n) { return $n != 3; }); // === true
-
-	Slash\any([], 'Slash\isOdd'); // === false
-
-	Slash\any([2, 4, 8], 'Slash\isOdd'); // === false
-
-	Slash\any((object) ['a' => 1, 'b' => 3, 'c' => 5], 'Slash\isOdd'); // === true
+ *	Slash\any([1, 3, 5], 'Slash\isOdd'); // === true
+ *
+ *	Slash\any([1, 3, 5], function ($n) { return $n != 3; }); // === true
+ *
+ *	Slash\any([], 'Slash\isOdd'); // === false
+ *
+ *	Slash\any([2, 4, 8], 'Slash\isOdd'); // === false
+ *
+ *	Slash\any((object) ['a' => 1, 'b' => 3, 'c' => 5], 'Slash\isOdd'); // === true
  */
 function any($array, $predicate)
 {
